8622027c521ed9fc65e82ecd760fd0c17b1558ee
Luke Schlessinger <lmschless@gmail.com>
2019-10-23 20:06:13 -0700

    Add files via upload
    Fantasy game inventory function. 
    
    Practice problem via Automate the Boring Stuff With Python Written by Al Sweigart

---
+++ fantasyInventory.py
@@ -0,0 +1,22 @@
+stuff = {'arrow':12,'gold coin':42,
+             'rope':1,'torch':6,'dagger':1}
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k,v in inventory.items():
+        print(str(v) + ' ' + k)
+        '''
+        ##  Commented out input functionality, will add in a future version.
+        newItem = input()
+        stuff[newItem]
+        print('Add quantity')
+        itemQuantity = input()
+        '''
+        item_total += v
+        print("Total number of items: " + str(item_total))
+        
+displayInventory(stuff)
+
+
+
